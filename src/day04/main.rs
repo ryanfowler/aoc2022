d5fc9e389980e58227e6137e986bbdd1c685cabd
Ryan Fowler <ryan.fowler19@gmail.com>
2022-12-06 05:15:25 +0000

    Add day04

---
+++ src/day04/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    println!("Day 04");
+    println!("======");
+
+    let input = include_str!("./input.txt");
+    let ans1 = input
+        .lines()
+        .map(parse_ranges)
+        .filter(|(r1, r2)| (r1.0 >= r2.0 && r1.1 <= r2.1) || (r2.0 >= r1.0 && r2.1 <= r1.1))
+        .count();
+    println!("Part 1: {}", ans1);
+
+    let ans2 = input
+        .lines()
+        .map(parse_ranges)
+        .filter(|(r1, r2)| (r1.0 >= r2.0 && r1.0 <= r2.1) || (r2.0 >= r1.0 && r2.0 <= r1.1))
+        .count();
+    println!("Part 2: {}", ans2);
+}
+
+fn parse_ranges(s: &str) -> ((i32, i32), (i32, i32)) {
+    let (first, second) = s.split_once(',').unwrap();
+    (parse_range(first), parse_range(second))
+}
+
+fn parse_range(s: &str) -> (i32, i32) {
+    let (low, high) = s.split_once('-').unwrap();
+    (low.parse::<i32>().unwrap(), high.parse::<i32>().unwrap())
+}
