4a8b180ad29a2fcec205e141d15bc561eb3dc6e8
Ryan Fowler <ryan.fowler19@gmail.com>
2022-12-07 06:35:38 +0000

    Add day07

---
+++ src/day07/main.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+
+fn main() {
+    println!("Day 07");
+    println!("======");
+
+    let input = include_str!("./input.txt");
+    let sizes = parse_sizes(input);
+
+    let ans1: i32 = sizes
+        .iter()
+        .filter(|(_, v)| **v <= 100_000)
+        .map(|(_, v)| *v)
+        .sum();
+    println!("Part 1: {}", ans1);
+
+    let to_free = *sizes.get("/").unwrap() - 40_000_000;
+    let ans2 = sizes.iter().fold(i32::MAX, |min, (_, &v)| {
+        if v >= to_free {
+            std::cmp::min(min, v)
+        } else {
+            min
+        }
+    });
+    println!("Part 2: {}", ans2);
+}
+
+fn parse_sizes(s: &str) -> HashMap<String, i32> {
+    let mut sizes = HashMap::new();
+
+    let mut path = Vec::new();
+    for line in s.lines() {
+        let split: Vec<&str> = line.split(' ').collect();
+        if split[0] == "$" {
+            // Handle changing directories.
+            if split[1] == "cd" {
+                if split[2] == ".." {
+                    path.pop();
+                } else {
+                    path.push(split[2]);
+                }
+            }
+        } else {
+            // Parse current directory
+            if split[0] != "dir" {
+                if let Ok(size) = split[0].parse::<i32>() {
+                    for i in 0..path.len() + 1 {
+                        *sizes.entry(path[..i].join("/")).or_insert(0) += size;
+                    }
+                }
+            }
+        }
+    }
+
+    sizes
+}
