730a95f18928f1e233399b6220139b41560ff63e
Ryan Fowler <ryan.fowler19@gmail.com>
2022-12-11 20:52:37 +0000

    Cleanup day11

--- src/day11/main.rs
+++ src/day11/main.rs
@@ -4,23 +4,25 @@ fn main() {
 
     let input = include_str!("./input.txt");
     let monkeys: Vec<Monkey> = input.split("\n\n").map(Monkey::parse).collect();
+    for (index, monkey) in monkeys.iter().enumerate() {
+        assert!(index == monkey.id);
+    }
 
-    let ans1 = monkey_business(monkeys.clone(), 20, true);
+    let ans1 = monkey_business(monkeys.clone(), 20, Some(3));
     println!("Part 1: {}", ans1);
 
-    let ans2 = monkey_business(monkeys, 10_000, false);
+    let ans2 = monkey_business(monkeys, 10_000, None);
     println!("Part 2: {}", ans2);
 }
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone)]
 enum Operation {
     Add(u64),
     Mul(u64),
-    #[default]
     Square,
 }
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone)]
 struct Monkey {
     id: usize,
     items: Vec<u64>,
@@ -32,35 +34,27 @@ struct Monkey {
 
 impl Monkey {
     fn parse(input: &str) -> Self {
-        let mut monkey = Monkey::default();
-        for (i, line) in input.lines().enumerate() {
-            match i {
-                0 => monkey.id = line[7..line.len() - 1].parse().unwrap(),
-                1 => {
-                    monkey.items = line[18..]
-                        .split(", ")
-                        .map(|i| i.parse::<u64>().unwrap())
-                        .collect()
-                }
-                2 => {
-                    monkey.op = match line[23..].split_once(' ').unwrap() {
-                        ("*", "old") => Operation::Square,
-                        ("*", n) => Operation::Mul(n.parse().unwrap()),
-                        ("+", n) => Operation::Add(n.parse().unwrap()),
-                        (op, _) => panic!("Unknown operator {}", op),
-                    }
-                }
-                3 => monkey.div = line[21..].parse().unwrap(),
-                4 => monkey.ift = line[29..].parse().unwrap(),
-                5 => monkey.iff = line[30..].parse().unwrap(),
-                _ => {}
-            }
+        let lines = input.lines().collect::<Vec<&str>>();
+        Monkey {
+            id: lines[0][7..lines[0].len() - 1].parse().unwrap(),
+            items: lines[1][18..]
+                .split(", ")
+                .map(|i| i.parse::<u64>().unwrap())
+                .collect(),
+            op: match lines[2][23..].split_once(' ').unwrap() {
+                ("*", "old") => Operation::Square,
+                ("*", n) => Operation::Mul(n.parse().unwrap()),
+                ("+", n) => Operation::Add(n.parse().unwrap()),
+                (op, _) => panic!("Unknown operator {}", op),
+            },
+            div: lines[3][21..].parse().unwrap(),
+            ift: lines[4][29..].parse().unwrap(),
+            iff: lines[5][30..].parse().unwrap(),
         }
-        monkey
     }
 }
 
-fn monkey_business(mut monkeys: Vec<Monkey>, rounds: u64, worry_relief: bool) -> u64 {
+fn monkey_business(mut monkeys: Vec<Monkey>, rounds: u64, worry_relief: Option<u64>) -> u64 {
     let mut activity = vec![0; monkeys.len()];
     let modulo: u64 = monkeys.iter().map(|m| m.div).product();
     for _ in 0..rounds {
@@ -72,11 +66,7 @@ fn monkey_business(mut monkeys: Vec<Monkey>, rounds: u64, worry_relief: bool) ->
                     Operation::Mul(n) => item * n,
                     Operation::Square => item * item,
                 };
-                let item = if worry_relief {
-                    item / 3
-                } else {
-                    item % modulo
-                };
+                let item = worry_relief.map_or_else(|| item % modulo, |v| item / v);
                 let next = if item % monkeys[i].div == 0 {
                     monkeys[i].ift
                 } else {
